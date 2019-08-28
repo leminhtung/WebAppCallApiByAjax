82aed6c4ed4be13a9bf41b02c4fbe697c65e0aef
Minh Tùng <leminhtung1111998@gmail.com>
2019-08-28 11:36:00 +0700

    New

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "characters between 3 and 50")]
+        [AllowHtml]
+        [RegularExpression("[a-zA-Z ]+$", ErrorMessage = "Enter Valid Name")]
+        [Required(ErrorMessage = "username can not be blank")]
+        public string Username { get; set; }
+
+        [StringLength(50)]
+        [Required(ErrorMessage = "PhoneNumber can not be blank")]
+        [RegularExpression(@"^(09|08[3|6|8|9])+([0-9]{7})$", ErrorMessage = "Phone number wrong format")]
+        public string PhoneNumber { get; set; }
+
+        [StringLength(50)]
+        //[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}
+        [DataType(DataType.EmailAddress)]
+        [RegularExpression("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "Enter Valid Name")]
+        [Required(ErrorMessage = "Email can not be blank")]
+        public string Email { get; set; }
+    }
+}
